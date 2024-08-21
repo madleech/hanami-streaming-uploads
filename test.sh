7c7ce137950134c9ad807520d7b8a74eb541304f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-08-21 14:19:28 +1200

    Run both tests.

--- test.sh
+++ test.sh
@@ -10,8 +10,6 @@ curl http://localhost:2300/create \
     -H 'Content-Type: video/mp4' \
     --data-binary @test.1b.txt
 
-exit
-
 echo "Testing with 20MB dummy file..."
 if [ ! -f test.20MB.mp4 ]; then
     dd if=/dev/zero of=test.20MB.mp4 bs=1M count=20 > /dev/null
